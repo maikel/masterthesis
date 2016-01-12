a8cd77472aadab46457a8f011ace8af6e58ed518
Maikel Nadolski <maikel.nadolski@gmail.com>
2016-01-12 17:00:58 +0100

    removed gnu files and changed period of sinus

--- Programme/sinus_const.m
+++ Programme/sinus_const.m
@@ -32,13 +32,20 @@ x = x0(1):dx:x0(2)-dx;
 a = velocity_const(x, 0.3);
 
 % initial values
-u0 = 0.8 * initial_value_sin_function(x, 2*pi);
+u0 = 0.8 * initial_value_sin_function(x, pi);
 
 % ============================================================================
 %                           Numerical Method
 % ============================================================================
 
 V = upwind(u0, a, cfl, N);
+
+% calculate reference solution
+U = zeros(size(V));
+U(1,:) = u0;
+for n = 2:N
+    U(n, :) = 0.8 * initial_value_sin_function(x-a*n*dt, pi);
+end
 
 % ============================================================================
 %                           Printing to File
@@ -47,7 +54,9 @@ V = upwind(u0, a, cfl, N);
 % Save data here and plot with gnuplot or tikz later
 
 V = [x' V'];
+U = [x' U'];
 a = [x' cfl*a'];
 
 save 'output/a_sin_const.dat' a
 save 'output/u_sin_const.dat' V
+save 'output/ref_sin_const.dat' U
